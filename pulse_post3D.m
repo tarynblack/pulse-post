f5555cdbb0b218a3299351f32a8f55bf4d7de115
Taryn Black <taryn.black@gatech.edu>
2015-12-02 22:37:00 -0500

    Cleaned up spacing and descriptions in run section and updated file description

--- pulse_post3D.m
+++ pulse_post3D.m
@@ -1,16 +1,16 @@
-%%% pulse_post3D processes data from MFiX simulations of 3D pulsating flow
-%%% in volcanic eruptions.
+%%% pulse_post3D processes output from simulations of 3D pulsating flow
+%%% in volcanic eruptions generated from a modified MFiX code base.
 %
-% For a set of MFiX simulations, where <allruns> are the jobIDs of the
-% simulations to be processed, pulse_post3D loads and manipulates MFiX
-% output data to produce movies and plots of the time evolution of gas
-% volume, gas temperature, gas pressure, and gas/solid momentum density
-% over the model domain for the duration of each simulation. Output figures
-% are saved to the same folder <run> that contains the relevant MFiX data
-% files. 
+% pulse_post3D loads and manipulates simulation output to produce movies,
+% plots, and calculation output of the time evolution of gas volume,
+% entrainment, particle concentration, gas temperature, flow density, and
+% flow buoyancy over the model domain for the duration of each simulation.
+% Figures are saved to the same folder <run> that contains the simulation
+% output files.
 %
-% Special functions called: setCnsts3D; calc_inletFlow; volume3D;
-%   entrainment3D; particleConc3D; gasTemperature3D; flowDensity3D
+% Functions called: setCnsts3D; calc_inletFlow; entrainment3D;
+% particleConc3D; gasTemperature3D; flowDensity3D 
+%
 % Last edit: Taryn Black, 2 December 2015
 
 clear all
@@ -22,10 +22,10 @@ clear all
 % Names of runs to be processed.
   runIDs = {'F_1_996_9999'};
   
-% Set path. Must end in / & contain dirs titled by runIDs being processed.
+% Path for directory containing post-processing data directories (runIDs).
   runpath = '~/data2/rundata/';
   
-% Set path for location of post-processing scripts.
+% Path for location of post-processing scripts.
   postpath = '~/data2/pulse-post';
 % ----------------------------------------------------------------------- %
 
@@ -44,7 +44,7 @@ clear all
 % ----------------------------------------------------------------------- %
   
 
-% --------------------- DEFINE PHYSICAL 'CONSTANTS' --------------------- %
+% --------------------- DEFINE 'PHYSICAL' CONSTANTS --------------------- %
 % Gas volume fraction that defines the boundary of the plume
   plumeedge = 1 - 1E-6;
   
@@ -115,6 +115,7 @@ clear all
 %%% =================================================================== %%%
 
 
+%%% CHECK FOR PROBLEMS BEFORE PROCESSING
 % Display error if color/transparency doesn't match # of isosurfaces:
   [Crow,Ccol] = size(colEPG);
   if Crow ~= length(isoEPG)
@@ -123,82 +124,81 @@ clear all
       error('Error. \nLength of trnEPG must match length of isoEPG.')
   end
   
-
+  
+%%% GENERATE FIGURES AND DO CALCULATIONS FOR EACH RUN
 for i = 1:length(runIDs)
     
-    run = runIDs{i};
-    sprintf('Now processing run %s',run)
-
-    dir = sprintf('%s%s',runpath,run);
-    titlerun = strrep(run,'_','\_');
+      run = runIDs{i};
+      disp(sprintf('Now processing run %s',run))
+      dir = sprintf('%s%s',runpath,run);
+      titlerun = strrep(run,'_','\_');
         
-  % Load and set constant simulation parameters
-    ghostcells = 4;     % MFiX adds these to each domain dimension
-    [IMAX,JMAX,KMAX,LENGTH,HEIGHT,WIDTH,RO_S1,RO_S2,RO_S3,NFR_S1,NFR_S2,...
-        NFR_S3,PULSE,FREQ,MING,MAXG,VENT_R,DT,TSTOP,ATMOS,TROPO,BC_EPG,...
-        BC_PG,BC_TG,BC_TS1,BC_TS2,BC_TS3] = setCnsts3D(run,dir,ghostcells,tstop);
-    cd(postpath)
-
-    timesteps = TSTOP/DT;
+    % Define post-processing constants from simulation parameters
+      ghostcells = 4;     % MFiX adds these to each domain dimension
+      [IMAX,JMAX,KMAX,LENGTH,HEIGHT,WIDTH,RO_S1,RO_S2,RO_S3,NFR_S1,...
+          NFR_S2,NFR_S3,PULSE,FREQ,MING,MAXG,VENT_R,DT,TSTOP,ATMOS,...
+          TROPO,BC_EPG,BC_PG,BC_TG,BC_TS1,BC_TS2,BC_TS3] ...
+          = setCnsts3D(run,dir,ghostcells,tstop);
+      cd(postpath)
+      timesteps = TSTOP/DT;
+      time = (0:timesteps)*DT;
     
-  % Define grid
-    X = LENGTH/(IMAX-ghostcells):LENGTH/(IMAX-ghostcells):LENGTH;
-    Y = HEIGHT/(JMAX-ghostcells):HEIGHT/(JMAX-ghostcells):HEIGHT;
-    Z = WIDTH/(KMAX-ghostcells):WIDTH/(KMAX-ghostcells):WIDTH;
-  % Dimension resolution [meters/cell], excluding ghost cells
-    XRES = LENGTH/(IMAX - ghostcells);
-    YRES = HEIGHT/(JMAX - ghostcells);
-    ZRES = WIDTH/(KMAX - ghostcells);
+    % Define domain grid and dimensional spatial resolution [meters/cell]
+      X = LENGTH/(IMAX-ghostcells):LENGTH/(IMAX-ghostcells):LENGTH;
+      Y = HEIGHT/(JMAX-ghostcells):HEIGHT/(JMAX-ghostcells):HEIGHT;
+      Z = WIDTH/(KMAX-ghostcells):WIDTH/(KMAX-ghostcells):WIDTH;
+      XRES = LENGTH/(IMAX - ghostcells);
+      YRES = HEIGHT/(JMAX - ghostcells);
+      ZRES = WIDTH/(KMAX - ghostcells);
         
-  % Set figure properties. Note: MFiX horizontal dimensions are LENGTH
-  % (x; MATLAB X) and WIDTH (z; MATLAB Y), vertical dimension is HEIGHT
-  % (y; MATLAB Z). tick* values are in meters.
-    tickx = linspace(0,LENGTH,Xpoints);
-    labelx = tickx(2:end)/Xfact;
-    ticky = linspace(0,HEIGHT,Ypoints);
-    labely = ticky(2:end)/Yfact;
-    tickz = linspace(0,WIDTH,Zpoints);
-    labelz = tickz(2:end)/Zfact;
-
-    time = (0:timesteps)*DT;
+    % Define tick locations and labels
+      tickx = linspace(0,LENGTH,Xpoints);
+      labelx = tickx(2:end)/Xfact;
+      ticky = linspace(0,HEIGHT,Ypoints);
+      labely = ticky(2:end)/Yfact;
+      tickz = linspace(0,WIDTH,Zpoints);
+      labelz = tickz(2:end)/Zfact;
       
-  % Calculate characteristic inlet velocity for use in entrainment script.
-    [XG,vel_char,MFR] = calc_inletFlow(charEPG,MING,MAXG,PULSE,BC_EPG,...
-        BC_PG,BC_TG,Rgas,RO_S1,RO_S2,RO_S3,NFR_S1,NFR_S2,NFR_S3,BC_TS1,...
-        BC_TS2,BC_TS3,VENT_R);
+    % Calculate characteristic inlet velocity
+      [XG,vel_char,MFR] = calc_inletFlow(charEPG,MING,MAXG,PULSE,BC_EPG,...
+          BC_PG,BC_TG,Rgas,RO_S1,RO_S2,RO_S3,NFR_S1,NFR_S2,NFR_S3,...
+          BC_TS1,BC_TS2,BC_TS3,VENT_R);
         
-  % Manipulate data to time evolution over domain and save output
-    vidEntr = entrainment3D(run,dir,vis,ghostcells,IMAX,JMAX,KMAX,...
-        tickx,labelx,labelXunit,ticky,labely,labelYunit,tickz,labelz,...
-        labelZunit,plumeedge,XRES,YRES,ZRES,postpath,PULSE,FREQ,time,...
-        vel_char,entrainment_cmin,entrainment_cmax,viewaz,viewel,imtype,...
-        titlerun,timesteps,isoEPG,colEPG,trnEPG,DT,VENT_R );
-    cd(postpath)
-
-    vidPartConc = particleConc3D(run,dir,vis,IMAX,JMAX,KMAX,ghostcells,...
-        tickx,labelx,labelXunit,ticky,labely,labelYunit,tickz,labelz,...
-        labelZunit,XRES,YRES,ZRES,postpath,sdistX,sdistY,sdistZ,...
-        particleConc_cmin,particleConc_cmax,titlerun,timesteps,PULSE,...
-        FREQ,time,imtype);
-    cd(postpath)
+    % Entrainment and gas volume fraction calculations and figures
+      entrainment3D(run,dir,vis,ghostcells,IMAX,JMAX,KMAX,tickx,labelx,...
+          labelXunit,ticky,labely,labelYunit,tickz,labelz,labelZunit,...
+          plumeedge,XRES,YRES,ZRES,postpath,PULSE,FREQ,time,vel_char,...
+          entrainment_cmin,entrainment_cmax,viewaz,viewel,imtype,...
+          titlerun,timesteps,isoEPG,colEPG,trnEPG,DT,VENT_R);
+      cd(postpath)
+      
+    % Particle concentration calculations and figures
+      particleConc3D(run,dir,vis,IMAX,JMAX,KMAX,ghostcells,tickx,labelx,...
+          labelXunit,ticky,labely,labelYunit,tickz,labelz,labelZunit,...
+          XRES,YRES,ZRES,postpath,sdistX,sdistY,sdistZ,...
+          particleConc_cmin,particleConc_cmax,titlerun,timesteps,PULSE,...
+          FREQ,time,imtype);
+      cd(postpath)
     
-    vidGasTemp = gasTemperature3D(run,dir,vis,ghostcells,IMAX,JMAX,KMAX,...
-        tickx,labelx,labelXunit,ticky,labely,labelYunit,tickz,labelz,...
-        labelZunit,XRES,YRES,ZRES,sdistX,sdistY,sdistZ,postpath,ATMOS,...
-        TROPO,Y,BC_TG,gasTemperature_cmin,PULSE,FREQ,time,titlerun,...
-        timesteps,imtype);
-    cd(postpath)
-        
-    vidFlowDens = flowDensity3D(run,dir,vis,IMAX,JMAX,KMAX,ghostcells,...
-        postpath,RO_S1,RO_S2,RO_S3,plumeedge,PULSE,FREQ,time,tickx,...
-        labelx,labelXunit,ticky,labely,labelYunit,tickz,labelz,...
-        labelZunit,XRES,YRES,ZRES,sdistX,sdistY,sdistZ,flowDensity_cmin,...
-        flowDensity_cmax,titlerun,flowBuoyancy_cmin,flowBuoyancy_cmax,...
-        timesteps,imtype);
-    cd(postpath)
+    % Gas temperature calculations and figures
+      gasTemperature3D(run,dir,vis,ghostcells,IMAX,JMAX,KMAX,tickx,...
+          labelx,labelXunit,ticky,labely,labelYunit,tickz,labelz,...
+          labelZunit,XRES,YRES,ZRES,sdistX,sdistY,sdistZ,postpath,ATMOS,...
+          TROPO,Y,BC_TG,gasTemperature_cmin,PULSE,FREQ,time,titlerun,...
+          timesteps,imtype);
+      cd(postpath)
+     
+    % Density and buoyancy calculations and figures  
+      flowDensity3D(run,dir,vis,IMAX,JMAX,KMAX,ghostcells,postpath,...
+          RO_S1,RO_S2,RO_S3,plumeedge,PULSE,FREQ,time,tickx,labelx,...
+          labelXunit,ticky,labely,labelYunit,tickz,labelz,labelZunit,...
+          XRES,YRES,ZRES,sdistX,sdistY,sdistZ,flowDensity_cmin,...
+          flowDensity_cmax,titlerun,flowBuoyancy_cmin,flowBuoyancy_cmax,...
+          timesteps,imtype);
+      cd(postpath)
     
-    close all
-
-%     clearvars -except i allruns
+      
+      close all
+      clearvars -except i allruns
 
 end
